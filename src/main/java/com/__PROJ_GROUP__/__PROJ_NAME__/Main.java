64d2e10f22be01faa6449c0d261dfe0661e39994
Noah Greff <greffnoah@gmail.com>
2022-09-17 15:01:48 +0200

    fixed `Main`

--- src/main/java/com/__PROJ_GROUP__/__PROJ_NAME__/Main.java
+++ src/main/java/com/__PROJ_GROUP__/__PROJ_NAME__/Main.java
@@ -1,19 +1,5 @@
-package com.than00ber.eru;
+package com.__PROJ_GROUP__.__PROJ_NAME__;
 
-import com.than00ber.eru.command.GroupCommand;
-import com.than00ber.eru.command.SpawnpointCommand;
-import com.than00ber.eru.command.StructureCommand;
-import com.than00ber.eru.core.manageable.BlockStructure;
-import com.than00ber.eru.core.manageable.Spawnpoint;
-import com.than00ber.eru.core.manageable.StructureGroup;
-import com.than00ber.eru.core.manager.GroupManager;
-import com.than00ber.eru.core.manager.SpawnpointManager;
-import com.than00ber.eru.core.manager.StructureManager;
-import com.than00ber.eru.event.SpawnpointEvents;
-import com.than00ber.eru.event.StructureEvents;
-import com.than00ber.eru.util.ConfigUtil;
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
