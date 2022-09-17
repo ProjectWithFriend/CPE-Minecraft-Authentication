b7967388da7226c0e7ddeb52710bfeb2cd447d9e
Noah Greff <greffnoah@gmail.com>
2022-09-17 14:18:44 +0200

    first commit

---
+++ src/main/java/com/__PROJ_GROUP__/__PROJ_NAME__/Main.java
@@ -0,0 +1,29 @@
+package com.than00ber.eru;
+
+import com.than00ber.eru.command.GroupCommand;
+import com.than00ber.eru.command.SpawnpointCommand;
+import com.than00ber.eru.command.StructureCommand;
+import com.than00ber.eru.core.manageable.BlockStructure;
+import com.than00ber.eru.core.manageable.Spawnpoint;
+import com.than00ber.eru.core.manageable.StructureGroup;
+import com.than00ber.eru.core.manager.GroupManager;
+import com.than00ber.eru.core.manager.SpawnpointManager;
+import com.than00ber.eru.core.manager.StructureManager;
+import com.than00ber.eru.event.SpawnpointEvents;
+import com.than00ber.eru.event.StructureEvents;
+import com.than00ber.eru.util.ConfigUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onLoad() {  }
+
+    @Override
+    public void onEnable() {  }
+
+    @Override
+    public void onDisable() {  }
+}
