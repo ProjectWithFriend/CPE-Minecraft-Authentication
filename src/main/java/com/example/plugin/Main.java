616361f7e6c45cf2bf53feb144c3ae4d7aed8c80
PMaw <peerapolth3327@hotmail.com>
2023-04-11 22:16:15 +0700

    add hello world

--- src/main/java/com/example/plugin/Main.java
+++ src/main/java/com/example/plugin/Main.java
@@ -1,18 +1,17 @@
 package com.example.plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.java.annotation.plugin.LogPrefix;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
-import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 @Plugin(name = "ExamplePlugin", version = "1.0")
-public final class Main extends JavaPlugin {
+public class Main extends JavaPlugin {
     @Override
-    public void onLoad() { }
-
-    @Override
-    public void onEnable() { }
-
-    @Override
-    public void onDisable() { }
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new MyListener(), this);
+    }
 }
+
